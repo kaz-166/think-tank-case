887654a32cf69dc301ce9da2b41f9e441ca4d8ac
kazuma <wanwan@super.com>
2020-05-20 23:24:21 +0900

    [add] 記事の内容の先頭3行を表示

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,21 @@
 module ArticlesHelper
+  # [Abstract]   複数行の文字列から先頭N行を取得するヘルパメソッド
+  # [Projection] f: (String, Integer) -> String
+  def article_subset(content, line)
+    s = content.split(/\r\n|\n|\r/)
+    s_out = ""
+    if line > s.length
+      for i in 0..s.length-1
+        s_out = s_out +  s[i]
+        s_out = s_out + "\n"      
+      end
+    else
+      for i in 0..line-1
+        s_out = s_out + s[i]
+        s_out = s_out + "\n"      
+      end
+      s_out = s_out + "..."
+    end
+    s_out
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
     # [Abstract]   PostモデルのExpressionの値から画像のPathに変換するヘルパメソッド
-    # [Projection] f: Integer-> String
+    # [Projection] f: Integer -> String
     # [Caution]    nameタグで渡されたパラメータの座標の存在性で判定しているためやや強引な方法
     def decode_expresssion(exp)
         if exp == 0
@@ -20,7 +20,7 @@ module PostsHelper
     end
 
     # 0～9ならば先頭に文字0を付加するメソッド
-    # [Projection] f: Integer-> String
+    # [Projection] f: Integer -> String
     def add_prefix(num)
       if (num >= 0) && (num <= 9)
         "0" + num.to_s
