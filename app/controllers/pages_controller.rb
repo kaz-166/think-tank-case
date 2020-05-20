70dec136cd92ad506dcbc556af074528b5d16fc2
kazuma <wanwan@super.com>
2020-05-20 17:33:19 +0900

    [add] Proceedingsのページネーションを実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
-  PER = 10
 
   def show
     @posts = User.find(current_user.id).posts.all.order(id: "DESC")
-    @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(PER) # ページネーション
+    @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(Settings.paginate_length.thinks) # ページネーション
     @user = User.find(current_user.id)
     current_project_id = @user.current_prj_id
     if current_project_id.nil? == false
       @project = @user.projects.find(current_project_id)
       @articles = @project.articles.all
+      @articles = Kaminari.paginate_array(@articles).page(params[:page]).per(Settings.paginate_length.proceeds)
     end
   end
 
